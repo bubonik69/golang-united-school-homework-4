d179b68caaf5b05ae05ba23f56d70e0d85b558e2
Artem <ruteboy69@gmail.com>
2022-05-21 16:41:23 +0300

    1

--- golang-united-school-homework-4/string_sum.go
+++ golang-united-school-homework-4/string_sum.go
@@ -2,10 +2,8 @@ package string_sum
 
 import (
 	"errors"
-	"fmt"
-	"strconv"
-	"strings"
 )
+
 //use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
@@ -23,44 +21,7 @@ var (
 // with fmt.Errorf function
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
+
 func StringSum(input string) (output string, err error) {
-		//func StringSum(input string) (output string, err error) {
-		// удаляем пробелы
-		var sum int
-		s:=strings.TrimSpace(input)
-		// если после удаления пробелов - длина строки нуль - пустое поле
-		if len(s)==0{
-			return "",fmt.Errorf("%e",errorEmptyInput)
-		}
-
-
-		term:=strings.Split(s,"+")
-		if len(term)==1{
-			// проводим дополнительную проверку на минусы
-			term=strings.Split(s,"-")
-			if len(term)>1{
-				for _,s:=range term {
-					if s!=""{
-						numer, err := strconv.Atoi(strings.TrimSpace(s))
-						sum -= numer
-						if err != nil {
-							return "", err
-						}}
-				}
-			}else {
-				return "",	errorNotTwoOperands
-			}
-
-		} else {
-			for _,s:=range term{
-				if s!="" {
-					numer, err := strconv.Atoi(strings.TrimSpace(s))
-					sum += numer
-					if err != nil {
-						return "", err
-					}}
-
-			}
-		}
-		return strconv.Itoa(sum),nil
-	}
+	return "", nil
+}
---
+++ string_sum_test.go
@@ -0,0 +1,23 @@
+package string_sum
+
+import (
+	"testing"
+)
+
+func TestStringSum (t *testing.T) {
+	tests:= []struct{
+		value	string
+		result string
+	}{
+		{"-3+5","2"},
+		{"-3-5","-8"},
+		{" 3 + 5 ","8"},
+	}
+	for i:=0;i<len(tests);i++{
+		if s,_:=StringSum(tests[i].value); s!=tests[i].result{
+			t.Errorf("Error in %d test value. Result is `%s`. Need `%s`",i,s,tests[i].result)
+		}else {
+			t.Logf("Test %d passed good",i)
+		}
+	}
+}

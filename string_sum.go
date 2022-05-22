a7b6fd3fe6f87fd8dd14d7cc5bf086a1bfa0fb94
Artem <ruteboy69@gmail.com>
2022-05-22 23:56:26 +0300

    4

--- string_sum.go
+++ string_sum.go
@@ -25,47 +25,62 @@ var (
 // with fmt.Errorf function
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
+
+func main(){
+	fmt.Println(StringSum("24-55"))
+}
+
 var re = regexp.MustCompile("[0-9]+")
 func StringSum (input string) (string, error) {
-		result, err := sum(input)
-	return result, err
+	//func StringSum(input string) (output string, err error) {
+	// удаляем пробелы
+	var sum int
+	s:=strings.ReplaceAll(input," ","")
+	// узнаем количество слагаемых
 
-}
-func sum(input string) (string, error) {
-	nums, err := findNums(input)
-	if err != nil {
-		return "", err
-	}
-	input = strings.Replace(input, " ", "", -1)
-	secondInput := strings.Index(input, nums[1])
-	firstVarStr := input[:secondInput-1]
-	firstVar, err := strToInt(firstVarStr)
-	if err != nil {
-		return "", fmt.Errorf("NumError")
-	}
-	secondVarStr := input[secondInput-1:]
-	secondVar, err := strToInt(secondVarStr)
-	if err != nil {
-		return "", fmt.Errorf("NumError")
-	}
-	return strconv.Itoa(firstVar + secondVar), nil
-}
 
-func findNums(input string) ([]string, error) {
-	nums := re.FindAllString(input, -1)
-	countOperands := len(nums)
-	switch {
-	case countOperands == 0:
-		return nil, errorEmptyInput
-	case countOperands != 2:
-		return nil, errorNotTwoOperands
+	// если после удаления пробелов - длина строки нуль - пустое поле
+	if len(s)==0{
+		return "", fmt.Errorf("1: %w", errorEmptyInput)
 	}
-	return nums, nil
-}
 
-func strToInt(input string) (int, error) {
-	intVar, err := strconv.Atoi(input)
-	if err != nil {
-		return 0, err
+	term:=strings.Split(s,"+") // делим строку по плюсам
+	if len(term)==2{
+		for _,s:=range term{
+			if s!="" {
+				numer, err := strconv.Atoi(strings.TrimSpace(s))
+				sum += numer
+				if err != nil {
+					return "", err
+				}}else {
+				return "",errorNotTwoOperands
+			}
+		}
 	}
-	return intVar, nil}
+	if len(term)>2{
+		return "", fmt.Errorf("2: %w", errorNotTwoOperands)
+	}
+
+	if len(term)==1{
+		// проводим дополнительную проверку на минусы
+		var re = regexp.MustCompile("[0-9]+")
+		nums := re.FindAllString(input, -1)
+		if len(nums)!=2{
+			return "", errorNotTwoOperands
+		}
+		firstInput := strings.Index(s, nums[0])
+		border := firstInput + len(nums[0])
+		firstTerm,err:=strconv.Atoi(strings.TrimSpace(s[:border]))
+		if err!=nil{
+			return "",err
+		}
+		secondTermerr,err:=strconv.Atoi(strings.TrimSpace(s[border:]))
+		if err!=nil{
+			return "",err
+		}
+		sum=firstTerm+secondTermerr
+	}
+
+
+	return strconv.Itoa(sum),nil
+}
